000baba3ea474cf2df2f84db0248de2df6964f30
psalmuelle <bjoestores@gmail.com>
2022-06-21 12:21:37 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict"
+
+const firstInteger = +prompt("Enter first digit(s):")
+
+const secondInteger = +prompt("Enter second digit(s):")
+
+const operation = prompt("What operation do you want to perform?  ( + ,  - ,  * ,  / )")
+
+
+if(operation === "+"){
+    alert(firstInteger + secondInteger)
+}else if(operation === "-"){
+    alert(firstInteger - secondInteger)
+}else if(operation === "*"){
+    alert(firstInteger * secondInteger)
+}else if(operation === "/"){
+    alert(firstInteger / secondInteger)
+}else{
+    alert("Wrong Operation!")
+}
